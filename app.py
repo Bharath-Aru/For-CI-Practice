7a60b8e113b960d882c28b387444f74aaaf76239
Bharath-Aru <130132024+Bharath-Aru@users.noreply.github.com>
2024-06-23 10:44:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hi da badus a small change'
+    return 'Hi da badus a sall change'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
